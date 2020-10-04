7620c17eb8bcc94e5ed747c25a2135451ec08f9e
TechnicalCoding <69109699+TechnicalCoding@users.noreply.github.com>
2020-10-04 12:58:05 +0530

    Add files via upload

---
+++ LED_on_and_off_by_using_bluetooth.ino
@@ -0,0 +1,21 @@
+void setup()
+{
+pinMode(13,OUTPUT);
+Serial.begin(9600);
+}
+void loop()
+{
+  if(Serial.available())
+  {
+    int z = Serial.read();
+    if(z==1)
+    {
+        digitalWrite(13,HIGH);
+    }
+    if(z==2)
+    {
+      digitalWrite(13,LOW);
+    }
+}
+}
+
